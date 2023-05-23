88e5453853858ea95fc46350439f887f87f7232b
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2023-05-22 22:05:14 -0300

    feat: delete customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CustomerService } from '../service/customer.service';
 import { Customer } from '../model/customer';
 import { DatePipe } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
 
   success: boolean = false;
   errors!: String[];
+  displayedColumns: string[] = ['idCustomer', 'firstNameCustomer', 'lastNameCustomer', 'cpfCustomer', 'birthdateCustomer', 'dateCreatedCustomer', 'monthlyIncomeCustomer', 'emailCustomer', 'statusCustomer', 'deleteCustomer'];
+  ELEMENT_DATA: Customer[] = [];
+  message: string = '';
+  dataSource = new MatTableDataSource<Customer>(this.ELEMENT_DATA);
 
-  constructor(private service: CustomerService){
-  }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private service: CustomerService
+  ){}
 
   ngOnInit(): void {
-    
+    this.listCustomer();
   }
 
   customer: Customer = {
@@ -56,6 +65,25 @@ export class CustomerComponent {
         //this.toast.error(ex.error.message, 'Atenção!');
       }
     }})
+  }
+
+  listCustomer() {
+    this.service.list().subscribe((response: any) => {
+      this.ELEMENT_DATA = response.result as Customer[]; // Verifique o tipo e faça a conversão
+      this.dataSource = new MatTableDataSource<Customer>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+    
+  }
+
+  deleteCustomer(customer: Customer) {
+    if (window.confirm('Deseja realmente excluir este contato?')) {
+      this.service.delete(customer.idCustomer).subscribe((response: any) => {
+        this.message = response.result.result as string;
+        window.alert(this.message);
+        this.listCustomer();
+      });
+    }
   }
 
 }
